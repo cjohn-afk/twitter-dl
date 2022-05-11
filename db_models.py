8ab46727784280d48fcf9e19af967329c096e01c
Christopher John <christopherjohn00@protonmail.com>
2022-05-11 17:45:05 -0400

    Fixed some issues preventing queries from executing.

--- db_models.py
+++ db_models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
-from sqlalchemy import DateTimes
+from sqlalchemy import DateTime
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -13,20 +13,20 @@ Base = declarative_base()
 class Account(Base):
     __tablename__ = "accounts"
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer)
 
 class Username(Base):
     __tablename__ = "usernames"
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     account_id = Column(Integer, ForeignKey("accounts.id"), nullable=False)
     username = Column(String(twitter_spec.max_username_length))
 
 class Post(Base):
     __tablename__ = "posts"
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     account_id = Column(Integer, ForeignKey("accounts.id"), nullable=False)
     post_id = Column(Integer)
     post_date = Column(DateTime)
--- db_queries.py
+++ db_queries.py
@@ -1,23 +1,30 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 import db_models
 
+from datetime import datetime
+
 from config import database_file
 engine = create_engine("sqlite+pysqlite:///" + database_file, echo=True, future=True)
-
-from datetime import datetime
+db_models.Base.metadata.create_all(engine)
 
 def reformat_date(twitter_date):
     return datetime.strftime(datetime.strptime(twitter_date,'%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d %H:%M:%S')
 
 def get_account_by_user_id(user_id):
-    return session.query(db_models.Account).filter(db_models.Acount.user_id == user_id).first()
-    
+    with Session(engine) as session:
+        return session.query(db_models.Account).filter(db_models.Account.user_id == user_id).first()
+
 def get_account_by_username(username):
     with Session(engine) as session:
-        account_id = session.query(db_models.Username).filter(db_models.Username.username == username).first().account_id
-        account = session.query(db_models.Account).filter(db_models.Acount.id == account_id).first()
+        uname = session.query(db_models.Username).filter(db_models.Username.username == username).first()
+        if uname is not None:
+            account_id = uname.account_id
+        else:
+            return None
+        account = session.query(db_models.Account).filter(db_models.Account.id == account_id).first()
         return account
 
 def get_posts_by_account_id(account_id):
@@ -28,19 +35,19 @@ def get_latest_post_date(account_id):
 
 def add_account(user_id, username):
     with Session(engine) as session:
-        new_account = Account(user_id = user_id)
+        new_account = db_models.Account(user_id = user_id)
         session.add(new_account)
         session.commit()
-    add_username(new_account.id, username)
+    add_username(get_account_by_user_id(user_id).id, username)
 
 def add_username(account_id, username):
     with Session(engine) as session:
-        new_username = Username(account_id = account_id, username = username)
+        new_username = db_models.Username(account_id = account_id, username = username)
         session.add(new_username)
         session.commit()
 
 def add_post(account_id, post_id, post_date, media_id):
     with Session(engine) as session:
-        new_post = Post(account_id = account_id, post_id = post_id, post_date = post_date, media_id = media_id)
+        new_post = db_models.Post(account_id = account_id, post_id = post_id, post_date = post_date, media_id = media_id)
         session.add(new_post)
         session.commit()
