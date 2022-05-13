3a2ede69f8301deaa45c074a96dceb1923c8e768
Christopher John <christopherjohn00@protonmail.com>
2022-05-13 16:11:34 -0400

    Removed camelCase in favor of more verbose naming.

--- twitdl.py
+++ twitdl.py
@@ -12,24 +12,24 @@ import db_queries
 
 from config import bearer_token
 
-totalRequests = 0
+total_requests = 0
 headers = CaseInsensitiveDict()
 headers["Authorization"] = "Bearer " + bearer_token
 
 def reformat_date(twitter_date):
     return  datetime.strptime(twitter_date,'%a %b %d %H:%M:%S +0000 %Y').replace(tzinfo=pytz.UTC)
 
-def requestTrimmer():
-    global totalRequests
+def request_trimmer():
+    global total_requests
     while True:
-        if totalRequests > 0:
-            totalRequests -= 1
+        if total_requests > 0:
+            total_requests -= 1
         sleep(1.15)
 
-def requestLimiter():
-    global totalRequests
+def request_limiter():
+    global total_requests
     while True:
-        if totalRequests < 20:
+        if total_requests < 20:
             break
         sleep(1.5)
 
@@ -44,15 +44,15 @@ def download(URL, location):
         print(URL)
         print(filename + " already exists. It will not be downloaded.")
         
-def downloadUserMedia(username):
-    global totalRequests
+def download_user_media(username):
+    global total_requests
     paginationToken = None
     
     account = db_queries.get_account_by_username(username)
     if account is None:
         print("No stored ID for " + username + ". Requesting ID...")
         response = requests.get('https://api.twitter.com/2/users/by/username/' + username, headers=headers)
-        totalRequests += 1
+        total_requests += 1
         requestLimiter()
         id = int(response.json()['data']['id'])
         db_queries.add_account(id, username)
@@ -71,19 +71,19 @@ def downloadUserMedia(username):
             pass
             ### TODO HANDLE CASE WHERE A FILE EXISTS AS THE USERNAME 
 
-    userDir = "./" + username + "/"
+    user_directory = "./" + username + "/"
 
     most_recent_post_date = db_queries.get_latest_post_date(db_queries.get_account_by_user_id(id).id)
 
-    paginationToken = -1
-    while paginationToken is not None:
+    pagination_token = -1
+    while pagination_token is not None:
         query = {'exclude':'retweets,replies', 'expansions':'attachments.media_keys', 'max_results':100}
-        if paginationToken != -1 and paginationToken is not None:
-            query["pagination_token"] = paginationToken
+        if pagination_token != -1 and pagination_token is not None:
+            query["pagination_token"] = pagination_token
 
         response = requests.get('https://api.twitter.com/2/users/'+str(id)+'/tweets', params=query, headers=headers)
-        totalRequests += 1
-        requestLimiter()
+        total_requests += 1
+        request_limiter()
         
         try:
             response.json()['data']
@@ -97,20 +97,20 @@ def downloadUserMedia(username):
         else:
             paginationToken = response.json()['meta']['next_token']
         
-        idList = []
+        id_list = []
         for tweet in response.json()['data']:
             try:
                 tweet['attachments']
             except:
                 pass
             else:
-                idList.append(tweet['id'])
+                id_list.append(tweet['id'])
 
-        for postID in idList:
-            query = {'id':postID, 'include_entities':'true', 'trim_user':'true','include_ext_alt_text':'false','include_my_retweet':'false','include_card_uri':'false'}
+        for post_id in id_list:
+            query = {'id':post_id, 'include_entities':'true', 'trim_user':'true','include_ext_alt_text':'false','include_my_retweet':'false','include_card_uri':'false'}
             response = requests.get('https://api.twitter.com/1.1/statuses/show.json', headers=headers, params=query)
             created_at = reformat_date(response.json()['created_at'])
-            totalRequests += 1
+            total_requests += 1
             requestLimiter()
             
             if most_recent_post_date is not None and created_at <= pytz.UTC.localize(most_recent_post_date):
@@ -129,17 +129,17 @@ def downloadUserMedia(username):
                     if m['type'] == 'video':
                         videos = m['video_info']['variants']
                         bitrate = 0
-                        videoURL = None
+                        video_url = None
                         for v in videos:
                             if v['content_type'] == 'video/mp4':
                                 if v['bitrate'] > bitrate:
                                     bitrate = v['bitrate']
-                                    videoURL = v['url']
-                        download(videoURL, userDir)
+                                    video_url = v['url']
+                        download(video_url, user_directory)
                     else:
-                        download(m['media_url_https']+"?name=orig", userDir)
+                        download(m['media_url_https']+"?name=orig", user_directory)
                         
-        if paginationToken is None:
+        if pagination_token is None:
             print("End of downloadable media from " + username + ".")
 
 def main(args):
@@ -147,14 +147,14 @@ def main(args):
 
     print(args)
 
-    rtThread = threading.Thread(target=requestTrimmer)
-    rtThread.daemon = True
-    rtThread.start()
+    request_trimmer_thread = threading.Thread(target=request_trimmer)
+    request_trimmer_thread.daemon = True
+    request_trimmer_thread.start()
 
     for username in args:
-        downloadUserMedia(username)
+        download_user_media(username)
         
-    totalRequests = -1
+    total_requests = -1
 
 main(sys.argv)
 sys.exit()
